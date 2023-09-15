08bc1183d88c64774c3406887e805fafe1347799
Kenneth Dimalibot <dimalibotkennethofficial@gmail.com>
2023-09-15 22:37:40 +0800

    server and adding env file

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
-const express = require('express');
-const colors = require('colors');
+//const express = require('express');
+//const colors = require('colors');
+
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+
+//configure env
+dotenv.config()
 
 //rest object
 const app = express();
 
 //rest api
 app.get("/", (req, res) => {
-    res.send({
-        message:'Welcome to Ecommerce App',
-    });
+    res.send('<h1>Welcome to IKennCode</h1>');
 });
 
 // declare PORT
 //this will create a localhost:8080 that will load a message from app.get
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 //run Listen
 //used back tick(``)
 app.listen(PORT, () => {
-    console.log(`Server Running On ${PORT}`.bgWhite.black);
+    console.log(
+        `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgWhite.black);
 });
