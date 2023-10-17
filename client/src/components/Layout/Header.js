4c5ba580f775cfefb421f0946689e386e940065a
Kenneth Dimalibot <dimalibotkennethofficial@gmail.com>
2023-10-17 20:42:26 +0800

    add build

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -30,7 +30,7 @@ const Header = () => {
     <>
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <Link to="/home" className="navbar-brand text-uppercase p-4">< GiShoppingCart/>KenSy Clothing</Link>
+    <Link to="/home" className="navbar-brand text-uppercase p-4">< GiShoppingCart/>AZT Fashion</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon" />
     </button>
